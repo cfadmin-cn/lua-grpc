ad281401c73cf8a119867fd7e5a94210f35adace
U-CANDYMI\Candy <869646063@qq.com>
2020-11-06 17:01:25 +0800

    初始化项目

---
+++ client.lua
@@ -0,0 +1,13 @@
+local class = require "class"
+
+local client = class("grpc-client")
+
+function client:ctor(opt)
+	-- body
+end
+
+function client:connect()
+	-- body
+end
+
+return client
---
+++ server.lua
@@ -0,0 +1,17 @@
+local class = require "class"
+
+local server = class("grpc-server")
+
+function server:ctor(opt)
+	-- body
+end
+
+function server:listen()
+	-- body
+end
+
+function server:run( ... )
+	-- body
+end
+
+return server
