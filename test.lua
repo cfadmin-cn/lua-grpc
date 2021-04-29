65616c82da5cb916f5aec3bc60bfe15a55eb2134
869646063@qq.com <869646063@qq.com>
2021-04-29 22:13:15 +0800

    更新测试代码

--- test.lua
+++ test.lua
@@ -1,17 +1,13 @@
 require "utils"
 
-local cf = require"cf"
+local cf = require "cf"
 
-local grpc = require "lua-grpc.io"
+local client = require "lua-grpc.client"
 
-local g = grpc:new( {
-  -- compressed = true,
-  domain = "http://localhost/"
-})
+local gclient = client:new { domain = "http://localhost/", max = 1 }
 
-g:load([[
+gclient:load([[
   syntax = "proto3";
-  package info;
   service Greeter {
     rpc SayHello(HelloRequest) returns (HelloReply);
     rpc SayHelloAgain(HelloRequest) returns (HelloReply);
@@ -20,26 +16,29 @@ g:load([[
   message HelloRequest {
     string name = 1;
   }
-
   message HelloReply {
     string message = 1;
   }
 ]])
 
-cf.fork(function()
-  local ret, err = g:call("Greeter", "SayHello", { name = "車先生" }, 5 --[[ 重试时间(秒) ]])
-  if not ret then
-    return print(ret, err)
+cf.fork(function ()
+  while true do
+    local info, err = gclient:call("Greeter", "SayHello", { name = "車先生" })
+    if not info then
+      return print(false, err)
+    end
+    var_dump(info)
+    cf.sleep(math.random())
   end
-  var_dump(ret)
 end)
 
-cf.fork(function()
-  local ret, err = g:call("Greeter", "SayHello", { name = "車爪鱼" }, 10 --[[ 重试时间(秒) ]])
-  if not ret then
-    return print(ret, err)
+cf.fork(function ()
+  while true do
+    local info, err = gclient:call("Greeter", "SayHelloAgain", { name = "車爪鱼" })
+    if not info then
+      return print(false, err)
+    end
+    var_dump(info)
+    cf.sleep(math.random())
   end
-  var_dump(ret)
 end)
-
-return require "cf".wait()
